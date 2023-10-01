b1e2ba639c7fb0a7bdf8d53ebac7b3b1235f4e0b
ijiejerry <143167220+ijiejerry@users.noreply.github.com>
2023-10-01 22:59:43 +0300

    first commit
    added empty variables

--- src/guessingGame/Guessinggame.java
+++ src/guessingGame/Guessinggame.java
@@ -1,9 +1,46 @@
 package guessingGame;
+import java.util.*;
+
 
 public class Guessinggame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String name="";
+		String ans = "";
+		int tries = 0;
+		
+		while (true) {
+			tries++;
+			System.out.println("guess a name: ");
+			name = "jerry";
+			ans = in.nextLine();
+			if(ans.equals(name)) {
+				System.out.println("Congratulations");
+				  System.out.println("Do you want to quit? (y/n)");
+
+				break;
+			}
+			
+			
+			else if(ans.equals("y")) {
+			 	System.out.println("guesse " + tries);
+				break;
+			}
+			else if(ans.equals("n")) {
+				continue;
+			}
+			
+			else {
+				  System.out.println("Do you want to quit? (y/n)");
+
+			}
+			
+			
+		}
+		
+		
 
 	}
 
